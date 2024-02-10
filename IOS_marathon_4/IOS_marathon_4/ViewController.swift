dc13fb96f0f8d5b3f9073a873a041f94b7b14e48
NatashaKonovalova <natasha@skladapp.com>
2024-02-10 12:31:25 +0300

    Base settings

---
+++ IOS_marathon_4/IOS_marathon_4/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  IOS_marathon_4
+//
+//  Created by Наталья Коновалова on 10.02.2024.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
